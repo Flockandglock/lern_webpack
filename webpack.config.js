41877440c6458ee0b7e767449c9c05aece8b2d1c
Denis <oggyqwe@gmail.com>
2024-08-02 18:15:08 +0400

    подключили html плагин

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
   return {
-		mode: env.mode ?? development,
-		entry: path.resolve(__dirname, 'src', 'index.js'),
-		output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  }
-	}
+    mode: env.mode ?? development,
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})],
+  };
 };
